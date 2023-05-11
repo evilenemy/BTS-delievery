59240dcbae3412142de78a323bae3cfeac145d9e
Evil Enemy <evilenemy55@gmail.com>
2023-05-11 23:02:29 +0500

    added template

--- item/views.py
+++ item/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
 
 def home(request):
-  return render(request, "item/home.html")
+  return render(request, "item/index.html")
+
+def about(request):
+  return render(request, "item/about.html")
+
+def contact(request):
+  return render(request, "item/contact.html")
